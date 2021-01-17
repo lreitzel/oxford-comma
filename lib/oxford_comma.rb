f1eb70046d5f518de7c477a378c990db93561e08
Lydia <lreitzel@gmail.com>
2021-01-17 14:01:49 -0800

    commit tree e6771e0a55b276b2f10c876d8727303dc5ed3f92

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+    array.join
 end
+
+def oxford_comma(two_array)
+    two_array.join(" and ")
+end
+
+def oxford_comma(three_array)
+    case three_array.size
+    when 0
+        ''
+    when 1
+        three_array.join
+    when 2
+        three_array.join(" and ")
+    else
+        three_array[-1] = "and #{three_array[-1]}"
+        three_array.join(", ")
+    end
+end
+
